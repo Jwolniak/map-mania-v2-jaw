f76bb0e7510c364144971efa1f8dee064fdf374b
Jacob <jacobwolniak@lewisu.edu>
2021-10-14 22:07:26 -0500

    reordered list of locations

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var myMap;
 
 var myLocations = [
-    {content:'#1: Crestwood,IL - My hometown', coordinates:{lat:41.6611, lng:-87.7526}, iconImagePath:"one.png"},
-    {content:'#2: Runaway Bay, Jamaica', coordinates:{lat:18.4563,lng:-77.3267}, iconImagePath:"two.png"},
-    {content:'Orland Park, IL', coordinates:{lat:41.6303, lng:-87.8539}, iconImagePath:"flag.png"},
-    {content:'Rhinelander, WI', coordinates:{lat:45.6366,lng:-89.4121}, iconImagePath:"flag.png"},
-    {content:'Romeoville, IL - Lewis', coordinates:{lat:41.6675, lng:-88.0895}, iconImagePath:"flag.png"},
-    {content:'Puerto Morelos, Mexico', coordinates:{lat:20.8478, lng:-86.8755}, iconImagePath:"flag.png"},
-    {content:'Portage, MI', coordinates:{lat:42.2012, lng:-85.5800}, iconImagePath:"flag.png"},
-    {content:'Grand Rapids, MI - Frederik Meijer Gardens', coordinates:{lat:42.9795, lng:-85.5856}, iconImagePath:"flag.png"},
-    {content:'Merryville, IN - Albanese Candy Factory', coordinates:{lat:41.4703, lng:-87.2700}, iconImagePath:"flag.png"},
     {content:'Cancun, Mexico', coordinates:{lat:21.1619, lng:-86.8515}, iconImagePath:"flag.png"},
+    {content:'Merryville, IN - Albanese Candy Factory', coordinates:{lat:41.4703, lng:-87.2700}, iconImagePath:"flag.png"},
+    {content:'Grand Rapids, MI - Frederik Meijer Gardens', coordinates:{lat:42.9795, lng:-85.5856}, iconImagePath:"flag.png"},
+    {content:'Portage, MI', coordinates:{lat:42.2012, lng:-85.5800}, iconImagePath:"flag.png"},
+    {content:'Puerto Morelos, Mexico', coordinates:{lat:20.8478, lng:-86.8755}, iconImagePath:"flag.png"},
+    {content:'Romeoville, IL - Lewis', coordinates:{lat:41.6675, lng:-88.0895}, iconImagePath:"flag.png"},
+    {content:'Rhinelander, WI', coordinates:{lat:45.6366,lng:-89.4121}, iconImagePath:"flag.png"},
+    {content:'Orland Park, IL', coordinates:{lat:41.6303, lng:-87.8539}, iconImagePath:"flag.png"},
+    {content:'#2: Runaway Bay, Jamaica', coordinates:{lat:18.4563,lng:-77.3267}, iconImagePath:"two.png"},
+    {content:'#1: Crestwood,IL - My hometown', coordinates:{lat:41.6611, lng:-87.7526}, iconImagePath:"one.png"},
 ];
 
 var locationIndex = 1;
@@ -21,7 +21,7 @@ function initApplication() {
     console.log("Welcome to Map Mania!");
 }
 function initMap() {
-    window.alert("Hello! Welcome to Jacob's Map Mania game! Here you will try to figure out\nmy 10 favorite places that I have been to. You can zoom in by double clicking on the map.\nHints will be given if you move away from the place you are looking for. There is a cheat code\nthat will give you an automatic win. Click on the Winner button to win.");
+    window.alert("Hello! Welcome to Jacob's Map Mania game! Here you will try to figure out\nmy 10 favorite places that I have been to. You can zoom in by double clicking on the map.\nHints will be given if you move away from the place you are looking for. There is a cheat code\nthat will give you an automatic win. Click on the Winner button to win.\nClick on the OK button when you are ready to begin.");
     hint = "All of my favorite places are located in North America.";
     myMap = new google.maps.Map(document.getElementById("myMapID"), {
         center: {lat: 0, lng: 0},
